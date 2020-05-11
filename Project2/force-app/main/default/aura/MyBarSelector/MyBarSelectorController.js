1aedeb8bee6783e61dd5aff08e194128439b7e67
pricem0595 <43712884+pricem0595@users.noreply.github.com>
2020-05-10 22:42:31 -0400

    Pre DualListBox

--- Project2/force-app/main/default/aura/MyBarSelector/MyBarSelectorController.js
+++ Project2/force-app/main/default/aura/MyBarSelector/MyBarSelectorController.js
@@ -1,10 +1,10 @@
 ({
     init: function (cmp, event, helper) {
-        var addingredient = [];
-        addingredient.push("Purple");
-        addingredient.push("Brown");
-        console.log(addingredient);
-        cmp.set('v.ingredients', addingredient);
+    //    var addingredient = [];
+    //    addingredient.push("Purple");
+    //    addingredient.push("Brown");
+    //    console.log(addingredient);
+    //    cmp.set('v.ingredients', addingredient);
 
         helper.ingredientCall(cmp);
      }
--- Project2/force-app/main/default/aura/MyBarSelector/MyBarSelectorHelper.js
+++ Project2/force-app/main/default/aura/MyBarSelector/MyBarSelectorHelper.js
@@ -5,11 +5,17 @@
         var data = null;
         
         var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
+        xhr.withCredentials = false;
         
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === this.DONE) {
-                console.log(this.responseText);
+                var ingredientList = [];
+                const obj = JSON.parse(this.responseText, (key, value) =>    {
+                  if (key == "strIngredient1"){
+                    ingredientList.push(value);
+                  }
+                });
+                cmp.set('v.ingredients', ingredientList);
             }
         });
         
@@ -20,63 +26,4 @@
 
 
 
-
-
-/*******************************************
-
-
-ingredientCall: function(cmp){
-var data = null;
-
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
-});
-
-xhr.open("GET", "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list");
-
-xhr.send(data);
-}
-*******************************************/
-
-
 })
-
-
-/*************************************
-public class CocktailCallout {
-    public Static String callout()
-    {
-  Http http = new Http();
-    HttpRequest request = new HttpRequest();
- 
-    request.setEndpoint('https://www.thecocktaildb.com/api.php');
-    request.setMethod('GET');
-    HttpResponse response = http.send(request);
-        System.debug(response.getStatusCode());
-        System.debug(response.getStatus());
-        return '';
-    }
-}
-
-
-ingredientCall : function(cmp) {
-        var action = cmp.get('c.getIngredients');
-        action.setCallback(this, $A.getCallback(function (response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                console.log(this.responseText);
-                //cmp.set('v.mydata', response.getReturnValue());
-            } else if (state === "ERROR") {
-                var errors = response.getError();
-                console.error(errors);
-            }
-        }));
-        $A.enqueueAction(action);
-    }
-
-*****************************************/
