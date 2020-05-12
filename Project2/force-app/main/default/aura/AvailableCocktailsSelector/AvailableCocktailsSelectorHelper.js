737b040a344b8d2cbd4a208a0793e4221188e223
pricem0595 <43712884+pricem0595@users.noreply.github.com>
2020-05-12 12:09:42 -0400

    Pixel Perfection

--- Project2/force-app/main/default/aura/AvailableCocktailsSelector/AvailableCocktailsSelectorHelper.js
+++ Project2/force-app/main/default/aura/AvailableCocktailsSelector/AvailableCocktailsSelectorHelper.js
@@ -16,16 +16,16 @@
     },
 
     getCocktailInfo: function(cmp, selected) {
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = false;
+        var info = new XMLHttpRequest();
+        info.withCredentials = false;
 
-        xhr.addEventListener("readystatechange", function () {
+        info.addEventListener("readystatechange", function () {
             if (this.readyState === this.DONE) {
-                cmp.set('v.drinkData', JSON.parse(this.response));
+                cmp.set('v.cocktailInfo', JSON.parse(this.response));
             }
         });
         
-        xhr.open("GET", "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + ingredientString);
-        xhr.send();
+        info.open("GET", "https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=" + selected);
+        info.send();
     }
 })
